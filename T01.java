27df2021f1847ccefd81b1714e4f17977e78155d
RimandaPanjaitan <panjaitanrimanda@gmail.com>
2024-09-04 21:27:52 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,15 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24049 - Rimanda Santa Risa Panjaitan
+// 12S24049 - Rimanda Santa Risa Panjaitan
+import java.util.*;
+import java.lang.Math;
 
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
